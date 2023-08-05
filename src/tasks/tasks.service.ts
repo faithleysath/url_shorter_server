f1aa2fdb98afb5e1c91f4f618def92039199510e
laysath <faithleysath@gmail.com>
2023-08-05 20:03:02 +0800

    :sparkles: feat: 增加了定期清理不常用链接的任务

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -8,12 +8,29 @@ export class TasksService {
   constructor(private readonly prisma: PrismaService) {}
   // 每分钟执行一次，删除ShortLink表中过期的数据，判断依据是expiresAt<now()
   @Cron('0 * * * * *')
-  async handleCron() {
+  async cleanExpireData() {
     this.logger.log('执行定时任务：删除过期的短链接');
     const res = await this.prisma.shortLink.deleteMany({
       where: {
         expiresAt: {
           lt: new Date(),
+        },
+      },
+    });
+    this.logger.log(res.count + '条数据被删除');
+  }
+
+  // 每天凌晨执行一次，删除有效期在一年后，但updatedAt在三个月前的数据
+  @Cron('1 0 0 * * *')
+  async cleanNoUseData() {
+    this.logger.log('执行定时任务：删除无用的短链接');
+    const res = await this.prisma.shortLink.deleteMany({
+      where: {
+        expiresAt: {
+          gt: new Date(Date.now() + 365 * 24 * 60 * 60 * 1000),
+        },
+        updatedAt: {
+          lt: new Date(Date.now() - 90 * 24 * 60 * 60 * 1000),
         },
       },
     });
